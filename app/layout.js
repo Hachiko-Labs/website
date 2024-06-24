eab4b0cafd700ca8a7300b51ddf6e61fa19794ca
Nicolle Valladares <nicollevalladares@gmail.com>
2024-06-24 17:29:58 -0600

    add navbar

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,8 @@
-import localFont from "next/font/local";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import { cn } from "@/lib/utils";
 import { getSEOTags } from "@/lib/seo";
 
 import Providers from "./providers";
 import "./globals.css";
-
-const neco = localFont({
-  variable: "--font-family-neco",
-  src: [
-    {
-      family: "Neco",
-      weight: "700",
-      path: "fonts/Neco-Bold.woff2",
-      style: "normal",
-    },
-    {
-      family: "Neco",
-      weight: "300",
-      path: "fonts/Neco-Medium.woff2",
-      style: "normal",
-    },
-    {
-      family: "Neco",
-      weight: "400",
-      path: "fonts/Neco-Regular.woff2",
-      style: "normal",
-    },
-  ],
-});
+import Navbar from "@/components/NavBar";
 
 export const viewport = {
   width: "device-width",
@@ -40,13 +13,13 @@ export const metadata = getSEOTags();
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={cn(GeistSans.className, GeistMono.variable, neco.variable)}
-    >
-      <body className="bg-background text-foreground font-geist-sans antialiased">
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body className="bg-background text-foreground font-geist-sans antialiased">
+          <Navbar />
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </>
   );
 }
---
+++ components/NavBar.js
@@ -0,0 +1,78 @@
+
+"use client";
+
+import Link from 'next/link';
+import Image from "next/image";
+import { useMemo, useState } from 'react';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const navItems = useMemo(
+        () => [
+            {
+                name: "Services",
+                href: "/"
+            },
+            {
+                name: "Projects",
+                href: "/"
+            },
+            {
+                name: "About",
+                href: "/"
+            },
+            {
+                name: "Work with us",
+                href: "/",
+                className: "border rounded-full border-black"
+            },
+        ], []
+    );
+
+    return (
+        <nav className="w-full h-20 fixed flex justify-center items-center backdrop-blur-xl bg-white sm:bg-white/80 z-40">
+            <div className="w-10/12 h-full flex items-center justify-between relative">
+                <a className="flex items-center gap-2" href="/" aria-label="Home">
+                    <div className="text-neutral font-bold flex gap-1">
+                        <div className="text-xl">Hachiko</div>
+                        <div className="text-primary text-xl">Labs</div>
+                    </div>
+                </a>
+                <div className="hidden lg:flex">
+                    {navItems.map((navItem) => (
+                        <a
+                            key={navItem.name}
+                            className={`flex items-center py-2 px-6 h-full ${navItem.className}`}
+                            href={navItem.href}
+                        >
+                            {navItem.name}
+                        </a>
+                    ))}
+                </div>
+            </div>
+
+            {isOpen && (
+                <div
+                    className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-white z-50 w-full 
+      items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
+      "
+                >
+                    {navItems.map(({ label, href, ariaLabel }) => (
+                        <a
+                            key={label}
+                            className="navbar-link"
+                            href={href}
+                            onClick={() => setIsOpen(false)}
+                            aria-label={ariaLabel}
+                        >
+                            {label}
+                        </a>
+                    ))}
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
